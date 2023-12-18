8250c053207cc6a3098ab6d421d33c285634e591
nabilhatami86 <nabilhatami27@gmail.com>
2023-12-18 21:25:45 +0700

    Pesan

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 // routes/authRoutes.js
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
-
+router.get('/login', authController.login);
+router.get('/register', authController.register)
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -6,6 +6,10 @@ const profileController = require('../controllers/profileController');
 
 const router = express.Router();
 
+router.get('/add-profile-picture' ,authMiddleware, profileController.addProfilePicture);
+router.get('/add-profile-picture:Id', authMiddleware, profileController.addProfilePicture)
+router.get('/update-profile', authMiddleware, profileController.updateProfile)
+router.get('/update-profile:Id', authMiddleware, profileController.updateProfile)
 router.post('/add-profile-picture',authMiddleware, profileController.addProfilePicture);
 router.put('/update-profile', authMiddleware, profileController.updateProfile);
 
